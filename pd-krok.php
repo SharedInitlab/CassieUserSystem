33cec708571881f3d0f797c8100bcfb218ba3702
Radoslav Georgiev <g2h.horde@gmail.com>
2012-07-01 17:44:38 +0300

    Removing the files that are not needed

--- pd-krok.php
+++ pd-krok.php
@@ -1,13 +1,19 @@
 <?php
-require 'config.php';
-
+require_once 'config.php';
 header('Content-Type: application/json');
 
 $tmp=`/home/vasil/bin/list2.sh|tr '\n' ',' |sed s/,$// `;
 
 
 $data = array();
-$sql ="SELECT users.id, name, url, twitter, privacy  FROM users LEFT JOIN objects ON users.id = objects.userid WHERE privacy = 0 AND objects.type='mac' AND objects.value IN (".$tmp.")";
+$sql ="SELECT users.id, name, url, twitter, privacy 
+		FROM users 
+		LEFT JOIN objects 
+			ON users.id = objects.userid 
+			WHERE privacy = 0 
+				AND objects.type='mac' 
+				AND objects.value IN (" . $tmp . ")";
+
 $res = mysql_query($sql);
 
 while ( $usr = mysql_fetch_assoc($res) )
